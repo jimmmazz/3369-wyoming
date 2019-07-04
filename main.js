bba53ec9a8d58eec016c2ed03b601741fc037fc9
Jim <Jimmmazz@gmail.com>
2019-07-04 10:32:43 -0400

    style-margin threshold height

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const sliders = document.querySelectorAll('.slide-in');
 const options = {
   root: null,
   rootMargin: '0px',
-  threshold: 0
+  threshold: 0.3
 };
 
 const observer = new IntersectionObserver(function(entries, observer) {
