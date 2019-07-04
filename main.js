cf0e0d7c24e920b3f8fdeaa78e484cd2fc0fa052
Jim <Jimmmazz@gmail.com>
2019-07-04 10:02:17 -0400

    obsrver threshold

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const sliders = document.querySelectorAll('.slide-in');
 const options = {
   root: null,
   rootMargin: '0px',
-  threshold: 0.15
+  threshold: 0
 };
 
 const observer = new IntersectionObserver(function(entries, observer) {
