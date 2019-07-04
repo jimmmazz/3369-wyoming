deba81c777812d52f7e48e9ac91bf01386ad9ebe
Jim <Jimmmazz@gmail.com>
2019-07-04 10:35:25 -0400

    styling

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const sliders = document.querySelectorAll('.slide-in');
 const options = {
   root: null,
   rootMargin: '0px',
-  threshold: 0.3
+  threshold: 0.25
 };
 
 const observer = new IntersectionObserver(function(entries, observer) {
