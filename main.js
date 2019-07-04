37551e73eab674b41470a15fb5ede2673410a75c
Jim <Jimmmazz@gmail.com>
2019-07-04 09:33:12 -0400

    init commit

---
+++ main.js
@@ -0,0 +1,25 @@
+const container = document.querySelector('.container');
+const slideImage = document.querySelectorAll('.image');
+const slideText = document.querySelectorAll('.text');
+const sliders = document.querySelectorAll('.slide-in');
+
+const options = {
+  root: null,
+  rootMargin: '0px',
+  threshold: 0.2
+};
+
+const observer = new IntersectionObserver(function(entries, observer) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add('appear');
+      observer.unobserve(entry.target);
+    }
+  });
+}, options);
+
+sliders.forEach(slider => {
+  observer.observe(slider);
+});
