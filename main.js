6392c0555475b2ae414136b91721bf9ce4052766
Jim <Jimmmazz@gmail.com>
2019-07-08 22:39:34 -0400

    hero

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
-// const container = document.querySelector('.container');
-// const slideImage = document.querySelectorAll('.image');
-// const slideText = document.querySelectorAll('.text');
+// Slide in from Left and Right
 const sliders = document.querySelectorAll('.slide-in');
 
 const options = {
@@ -15,10 +13,10 @@ const slideInObserver = new IntersectionObserver(function(
 ) {
   entries.forEach(entry => {
     if (!entry.isIntersecting) {
-      console.log(entry);
+      // console.log(entry);
       return;
     } else {
-      console.log(entry);
+      // console.log(entry);
       entry.target.classList.add('appear');
       slideInObserver.unobserve(entry.target);
     }
@@ -27,6 +25,42 @@ const slideInObserver = new IntersectionObserver(function(
 options);
 
 sliders.forEach(slider => {
-  console.log(slider);
+  // console.log(slider);
   slideInObserver.observe(slider);
 });
+
+//Event listener ffor gallery scroll
+
+document.querySelector('.gallery').addEventListener('click', function(e) {
+  const whatClicked = e.target;
+  console.log(whatClicked);
+  if (whatClicked.nextElementSibling === null) {
+    whatClicked.style.order = '0';
+  } else {
+    whatClicked.nextElementSibling.style.order = '-1';
+    whatClicked.style.order = '0';
+  }
+});
+
+// function scrollSlide(e) {
+//   const whatClicked = e.target;
+
+//   if (whatClicked) {
+//     let i = 0;
+
+//     x.forEach(slide => {
+//       i++;
+
+//       if (i === x.length) {
+//         console.log(i);
+//         slide.style.order = x.length - 1;
+//         console.log('end');
+//       } else {
+//         // console.log(i);
+//         console.log(i);
+//         whatClicked.style.order = x.length;
+//         slide.style.order = i;
+//       }
+//     });
+//   }
+// }
